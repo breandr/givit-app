df6e0cd20d2df12c0526582d90231bad9e7af08f
Brett Andrews <brett.j.andrews@gmail.com>
2014-03-11 09:14:41 +1000

    added ionic grunt file for reference; added donate item

---
+++ app/scripts/services/donateitem.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('givitApp')
+  .service('Donateitem', function Donateitem() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ test/spec/services/donateitem.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: Donateitem', function () {
+
+  // load the service's module
+  beforeEach(module('givitAppApp'));
+
+  // instantiate service
+  var Donateitem;
+  beforeEach(inject(function (_Donateitem_) {
+    Donateitem = _Donateitem_;
+  }));
+
+  it('should do something', function () {
+    expect(!!Donateitem).toBe(true);
+  });
+
+});
