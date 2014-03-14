57113d4649fdfa66f71250e97f9bbff6f47f72da
Brett Andrews <brett.j.andrews@gmail.com>
2014-03-14 10:01:52 +1000

    added intro flag to local storage defaults

--- app/scripts/controllers/give-item.js
+++ app/scripts/controllers/give-item.js
@@ -4,10 +4,11 @@ angular.module('givitApp')
   .controller('GiveItemCtrl', function ($scope, $routeParams, GivitList) {
     var itemGuid = $routeParams.itemGuid;
 
-    // change this to loop through items and find it
     $scope.item = _.find(GivitList.items, function (item) {
       return item.guid === itemGuid;
     });
 
-    console.log($scope.item);
+    if(!$scope.item){
+    	// reroute to givit-list
+    }
   });
--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -5,7 +5,15 @@ angular.module('givitApp')
     this.$storage = $localStorage;
     this.$storage.$default({
       userDetails: {},
-      givitListSearch: {}
+      givitListSearch: {},
+      // nav: Navigate the app from here
+      // <open nav to explain each page>
+      // givit list: See what items charities have requested for someone in need
+      // donate items: Tell us of items you have that you would like to give to someone in need
+      // my details: Enter your details so we can contact you when giving
+      // about: Want to know more about Givit?
+      // <set showIntro false>
+      showIntro: true
     });
 
     this.setUserDetails = function (userDetails) {
