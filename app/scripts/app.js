181e8cc18a00c7dbf4c3c03cfd695f2a5321550a
Brett Andrews <brett.j.andrews@gmail.com>
2014-03-14 18:18:01 +1000

    x

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,33 +13,33 @@ angular.module('givitApp', [
     function ($routeProvider, $locationProvider) {
       $routeProvider
         .when('/', {
-          // templateUrl: '/views/main.html',
+          // templateUrl: 'views/main.html',
           // controller: 'MainCtrl'
-          templateUrl: '/views/givit-list.html',
+          templateUrl: 'views/givit-list.html',
           controller: 'GivitListCtrl'
         })
         .when('/givit-list', {
-          templateUrl: '/views/givit-list.html',
+          templateUrl: 'views/givit-list.html',
           controller: 'GivitListCtrl'
         })
         .when('/donate-item', {
-          templateUrl: '/views/donate-item.html',
+          templateUrl: 'views/donate-item.html',
           controller: 'DonateItemCtrl'
         })
         .when('/user-details', {
-          templateUrl: '/views/user-details.html',
+          templateUrl: 'views/user-details.html',
           controller: 'UserDetailsCtrl'
         })
         .when('/about', {
-          templateUrl: '/views/about.html',
+          templateUrl: 'views/about.html',
           controller: 'AboutCtrl'
         })
         .when('/terms-of-participation', {
-          templateUrl: '/views/terms-of-participation.html',
+          templateUrl: 'views/terms-of-participation.html',
           controller: 'TermsOfParticipationCtrl'
         })
         .when('/give-item/:itemGuid', {
-          templateUrl: '/views/give-item.html',
+          templateUrl: 'views/give-item.html',
           controller: 'GiveItemCtrl'
         })
         .otherwise({
